e0f64860d6276d8c0feadead9032a28b08c52d1c
Alexchandrahanif <alexchandra1998@gmail.com>
2022-12-11 14:39:12 +0700

    okey2

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const cors = require("cors");
 const router = require("./router");
 const handleError = require("./midleware/handleError");
+
+const cors = require("cors");
+const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
